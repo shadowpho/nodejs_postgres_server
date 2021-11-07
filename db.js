51e3f2e0f7e00a66446bdaa56015e5606d01f610
George Karavaev <shadowp@gmail.com>
2021-11-07 11:25:44 -0800

    first revision it works ty magic person

--- db.js
+++ db.js
@@ -1,18 +1,31 @@
-const config = { user: 'node_js', host: 'database', database: 'sensors', port:5432,  ssl: {rejectUnauthorized: false,},}
+const config = { user: 'node_js', host: 'database', database: 'sensors', port: 5432, ssl: { rejectUnauthorized: false, }, }
 const { Pool, Client } = require('pg')
 const pool = new Pool(config)
 
-
+/*
 pool.query('SELECT * FROM rpi_sensor limit 1;', (err, res) => {
     if (err) { throw err }
-    console.log(' ', res.rows[0]) })
+    console.log(' ', res.rows[0])
+})
+*/
 
-
-
-
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
 module.exports = {
-    query: (text, params, callback) => {
-        return pool.query(text, params, callback)
+    db_store: (text) => {
+        return new Promise(async (success, failed) => {
+            await sleep(1000*60);
+            /*0);
+            pool.query(text, (err, res) => {
+                if (err) { 
+                    failed(err);
+                }
+                success(res.rows[0]);
+            })
+            */
+            success("10.0");
+        });
     },
-    }
+}
--- nodejs_postgres_server.js
+++ nodejs_postgres_server.js
@@ -1,30 +1,44 @@
-var http = require('http'); 
-
+var http = require('http');
+var db = require('./db');
 
 var temp = "0.0"
-var server = http.createServer(function (req, res) {   
 
-	    if (req.method == "GET") {
-		    res.writeHead(200, {
-			    "Content-Type": "application/json",  
-		    	    "Access-Control-Allow-Origin":"*"});
-		    res.write(JSON.stringify({temperature: temp}));
-		    res.end();
-	    } else if (req.method == "POST") {
-	    	   var body = "";
-		   req.on("data", function(chunk) {
-			   body+=chunk;
-	    	   });
-		   req.on("end", function(){
-			   const data = JSON.parse(body);
-			   temp = data.sensor_data.temperature;
-			   console.log(temp);
-			   res.writeHead(200, {"Content-Type": "text/plan"});
-			   res.end("SUCCESS");
-		   });
-	}		
+const async_db_interaction = async (body, res) => {
+	var response = "SUCCESS";
+	try {
+		const data = JSON.parse(body);
+		temp = data.sensor_data.temperature;
+		console.log(temp);
+	} catch (error) { 
+		console.error(error)
+		response = "FAILED TO PARSE JSON";
+	};
+
+	var result = await db.db_store(body);
+	console.log(result);
+
+	res.writeHead(200, { "Content-Type": "text/plan" });
+	res.end(response);
+};
+
+var server = http.createServer(function (req, res) {
+	if (req.method == "GET") {
+		res.writeHead(200, {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*"
+		});
+		res.write(JSON.stringify({ temperature: temp }));
+		res.end();
+	} else if (req.method == "POST") {
+		var body = "";
+		req.on("data", function (chunk) {
+			body += chunk;
+		});
+		req.on("end", function () {
+			async_db_interaction(body, res);
+		});
+	}
 });
-	
-//server.listen(4050); 
-	//
-//console.log('Node.js web server at port 4049 is running..')
+
+server.listen(4050);
+console.log('Node.js web server at port 4050 is running..')
