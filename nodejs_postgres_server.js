8a920245909ed582c978b32567c0a9c8e81d4ee5
George Karavaev <shadowp@gmail.com>
2022-01-15 21:01:55 -0800

    added logging function

--- nodejs_postgres_server.js
+++ nodejs_postgres_server.js
@@ -47,6 +47,16 @@ var server = http.createServer(function (req, res) {
 		res.write(JSON.stringify({ temperature: temp }));
 		res.end();
 
+	} else if (req.method === "POST" && request.url === '/logs'){
+		var body = "";
+		req.on("data", function (chunk) {
+			body += chunk;
+		});
+		req.on("end", function () {
+			console.log(body);
+			res.writeHead(200, { "Content-Type": "text/plan" });
+			res.end("SUCCESS");
+		});
 	} else if (req.method === "POST") {
 		var body = "";
 		req.on("data", function (chunk) {
